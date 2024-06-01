36fb0d6aafac5f28ff2852a1eb54c6ee228784a1
AJ <113120945+SmeaGL@users.noreply.github.com>
2024-06-01 22:52:28 +0800

    major changes added functionality to register
    added a new input:user-type to register fixed some errors within the code and added functionality to register user

--- MCO_LoginPage/loginscript.js
+++ MCO_LoginPage/loginscript.js
@@ -1,22 +1,49 @@
-const wrapper = document.querySelector('.wrapper');
-const loginlink = document.querySelector('.login-link');
-const registerlink = document.querySelector('.reg-link');
-const btnLoginpopup = document.querySelector('.btnLogin-popup');
+$(document).ready(function() {
+/** Create class person and funtions for web app functionality */
+class Person {
+    constructor(username, email, password, usertype) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.usertype = usertype;
+    }
+}
+const $registerSubmit = $('.register-submit');
 
-registerlink.addEventListener('click', () => {
-    wrapper.classList.add('action');
+$registerSubmit.on('click', function() {
+    const username = $('#reg-username').val();
+    const email = $('#reg-email').val();
+    const password = $('#reg-password').val();
+    const usertype = $('#user-type').val();
+
+    const person = new Person(username, email, password, usertype);
+    
+    console.log(person);
 });
 
-loginlink.addEventListener('click', () => {
-    wrapper.classList.remove('action');
+/**for css html functions*/
+
+const $wrapper = $('.wrapper');
+const $loginlink = $('.login-link');
+const $registerlink = $('.reg-link');
+const $btnLoginpopup = $('.btnLogin-popup');
+
+$registerlink.on('click', function() {
+    $wrapper.addClass('action');
 });
 
-btnLoginpopup.addEventListener('click', () => {
-    if (wrapper.classList.contains('action-popup')) {
-        wrapper.classList.remove('action-popup');
+$loginlink.on('click', function() {
+    $wrapper.removeClass('action');
+});
+
+$btnLoginpopup.on('click', function() {
+    if ($wrapper.hasClass('action-popup')) {
+        $wrapper.removeClass('action-popup');
         console.log('removed action-popup');
     } else {
-        wrapper.classList.add('action-popup'); 
+        $wrapper.addClass('action-popup');
         console.log('added action-popup');
     }
-})
+});
+
+});
