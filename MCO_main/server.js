8211d564bb982221ca2161dfafe2ac386df38f19
AJ <113120945+SmeaGL@users.noreply.github.com>
2024-08-02 16:38:41 +0800

    minor changes

--- MCO_main/server.js
+++ MCO_main/server.js
@@ -144,7 +144,7 @@ app.get("/profile", isAuthenticated, async (req, res) => {
 
     if (email) {
       const response = await fetch(
-        `http://localhost:3000/api/userProfileOther?email=${email}`
+        `http://ccapdev-labreserve.onrender.com/api/userProfileOther?email=${email}`
       );
 
       if (!response.ok) {
